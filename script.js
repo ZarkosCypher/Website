123c94da32fac411dd7c7f08bb406f9b1ccc6cb2
ZarkosCypher <146312357+ZarkosCypher@users.noreply.github.com>
2024-12-02 14:59:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 
-document.addEventListener("mousemove", (event) => {
-    const dot = document.createElement("div");
-    dot.className = "dot";
-
-    // Set position of the dot to match the mouse
-    dot.style.left = `${event.pageX}px`;
-    dot.style.top = `${event.pageY}px`;
-
-    // Add dot to the background
-    document.body.appendChild(dot);
-
-    // Remove the dot after a short delay
-    setTimeout(() => {
-        dot.remove();
-    }, 2000);
-});
