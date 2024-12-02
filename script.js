58306cc530c4be683c3a0ab2ed9b7d566f2eca46
ZarkosCypher <146312357+ZarkosCypher@users.noreply.github.com>
2024-12-02 15:12:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+window.onscroll = function () {
+    let button = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        button.style.display = "block";
+    } else {
+        button.style.display = "none";
+    }
+};
+
+document.getElementById("back-to-top").onclick = function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+};
